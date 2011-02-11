9bf371035b10709c60fc272f316cfefc499d4c1d
Nicholas Rutherford <nick.rutherford@gmail.com>
2011-02-12 00:46:45 +0100

    just mendeley

--- timeline.rb
+++ timeline.rb
@@ -3,36 +3,11 @@ require 'rest-client'
 require 'haml'
 require 'json'
 
-DEFAULT_QUERY_SIZE = 100
+DEFAULT_QUERY_SIZE = 200
 
 START_YEAR = 1900
 END_YEAR = Time.now.year
 
-get '/guardian/:search_term/from/:start/to/:end' do |search_term, start_year, end_year|
-  item_count = params['results'] || DEFAULT_QUERY_SIZE
-  results = Timeline.query_yql %Q(select * from guardian.content.search(#{item_count}) where q="#{search_term}" and from-date="#{start_year}-01-01" and to-date="#{end_year}-01-01")
-  news = results['results']['content']
-
-  haml :'news/timeline', :locals => {
-    :timeline_data => Timeline.map_news_json_to_timeline_json(news),
-    :search_term => search_term,
-    :items_found => results['count'],
-    :news => news
-  }
-end
-
-get '/news/:search_term' do |search_term|
-  item_count = params['results'] || DEFAULT_QUERY_SIZE
-  results = Timeline.query_yql %Q(select * from guardian.content.search(#{item_count}) where q="#{search_term}" and from-date="1960-01-01" and to-date="1970-01-01")
-  news = results['results']['content']
-
-  haml :'news/timeline', :locals => {
-    :timeline_data => Timeline.map_news_json_to_timeline_json(news),
-    :search_term => search_term,
-    :items_found => results['count'],
-    :news => news
-  }
-end
 
 get '/papers/:search_term' do |search_term|
   item_count = params['results'] || DEFAULT_QUERY_SIZE
@@ -40,10 +15,23 @@ get '/papers/:search_term' do |search_term|
   results = results['results']['json']
   papers = results['documents']
 
+  total_pages = results['total_pages'].to_i
+  if total_pages > 0
+    pages = (1)..(4)
+    pages.each do |page|
+      query = %Q(select * from mendeley.search where query="#{search_term}" and items="#{item_count}" and page="#{page}")
+      puts query
+      more = Timeline.query_yql query
+      papers += more['results']['json']['documents']
+    end
+  end
+  
+  timeline_json = Timeline.map_mendeley_json_to_timeline_json(papers)
+  
   haml :'papers/timeline', :locals => {
-    :timeline_data => Timeline.map_mendeley_json_to_timeline_json(papers),
+    :timeline_data => timeline_json,
     :search_term => search_term,
-    :items_found => "#{results['items_per_page']} of #{results['total_results']} (page #{results['current_page']} of #{results['total_pages']})",
+    :items_found => "#{timeline_json.length} of #{results['total_results']} (page #{results['current_page']} of #{results['total_pages']})",
     :papers => papers
   }
 end
@@ -62,19 +50,8 @@ module Timeline
     JSON.parse(json)['query']
   end
 
-  def self.map_news_json_to_timeline_json(json)
-    json.map do |element|
-      n = {}
-      n['start'] = element['web-publication-date']
-      n['title'] = element['web-title']
-      n['description'] = element['web-title']
-      n['link'] = element['web-url']
-      n
-    end
-  end
-
   def self.map_mendeley_json_to_timeline_json(json)
-    json.delete_if {|n| (n['year'].to_i < START_YEAR) or (n['year'].to_i > END_YEAR)  }
+    json.delete_if {|n| begin n['year'].to_i == 0 rescue true end }
     json.map do |element|
       n = {}
       n['start'] = element['year']
